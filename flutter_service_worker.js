53da3eba8c962338756ec2c0538f4b88acf64b7a
Birjis Khatoon <birjiskhatoon213@gmail.com>
2025-10-03 00:15:09 +0530

    Automated web deployment update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -49,7 +49,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b53932998a631ddf9731b5e03b7245d5
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a33de018d3ffe5e3fc2e4a6a040e671c",
+"flutter_bootstrap.js": "21f52376a9a5b32938362f11d9788820",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
